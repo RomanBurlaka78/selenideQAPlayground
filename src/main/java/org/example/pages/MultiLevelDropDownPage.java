acb54462ef8c8c8583537ee6d4a0afdc6bf10b09
Roman Burlaka <burlakarv@gmail.com>
2025-05-11 18:43:23 +0200

    MultiLOevelDropdownTests

---
+++ src/main/java/org/example/pages/MultiLevelDropDownPage.java
@@ -0,0 +1,26 @@
+package org.example.pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static java.nio.file.Files.size;
+
+public class MultiLevelDropDownPage extends BasePage<MultiLevelDropDownPage> {
+    ElementsCollection icons = $$("a[class ='icon-button']");
+
+    ElementsCollection meuItem = $$("a[class ='menu-item']");
+
+    @Step("check dropdown {name} {items}")
+    public MultiLevelDropDownPage getDropdown(String name, String items) {
+        SelenideElement shevron = icons.get(3);
+        shevron.click();
+        meuItem.find(Condition.innerText(name)).click();
+        meuItem.shouldHave().find(Condition.innerText(items)).click();
+
+        return this;
+    }
+}
--- src/main/java/org/example/pages/OpenNewTabPage.java
+++ src/main/java/org/example/pages/OpenNewTabPage.java
@@ -1,5 +1,6 @@
 package org.example.pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
@@ -8,16 +9,20 @@ import java.time.Duration;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.*;
 
-public class OpenNewTabPage extends BasePage<OpenNewTabPage>{
+public class OpenNewTabPage extends BasePage<OpenNewTabPage> {
+    SelenideElement tab = $("a[id='open']");
 
-    @Step("open new tab : {name}")
+    @Step("open new tab : {title}")
     public OpenNewTabPage openNewTabPage(String title) {
-      SelenideElement tab = $("a[id='open']");
-      tab.click();
-      String name = tab.getText();
+        tab.click();
+        String name = tab.getText();
+        switchTo().window(title);
 
-      switchTo().window(title);
+        return this;
+    }
 
-      return this;
+    @Step("color should be same as: {color}")
+    public void colorShouldBeSameAs(String color) {
+        tab.hover().shouldHave(Condition.cssValue("background", color));
     }
 }
